02d54f505c7d6bfc54385d2faf8a6e40fff3540e
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-17 04:36:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3);
+                       // code readability should be high
+console.log("kunal");
+
+let name = "kunal"
+let age = 18
+let isloggedIn = false
+let state;
+
+//number => 2 to power 52
+//bigInt => used for large value like for stockmarket values
+//String => ""
+//Boolean => true or false
+//null => standalone value
+//undefines => for discuss about the unique
+
+
+//object
+
+console.log(typeof isloggedIn);
+console.log(typeof null); //null => object
+console.log(typeof undefined); // undefined
+
+
+
+
