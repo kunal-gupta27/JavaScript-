dd963679aa5c9da0f32c8fcb662221f786e386a9
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-19 04:53:01 +0000

    operations

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -22,8 +22,57 @@ let somenumber = 33
 
 let stringnumber = String(somenumber)
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+//===========================   OPERATIONS   ==============================================
+
+
+let value = 2
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //Power
+// console.log(2/2); //divide
+// console.log(2%2); //remainder
+
+let str1 = "Hello"
+let str2 = " kunal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1"+2+3);   // agar string first hain too sabko ek sath print kr deti hain
+// console.log(1+2+"3");
+// console.log(1+2+"3"+4);  // agar string kisi no. k baad hain too too string sai pahle vaale no. ko add kr deti haibn aur baad vaalo ko same utar deti hain
+
+// console.log(+true);
+// console.log(+"");  // not well method
+
+
+//Assignment operator
+
+let gamecounter = 100;
+// gamecounter++; //postfix
+// ++gamecounter;  // prefix
+console.log(gamecounter);
+
+//link to study => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+
+
+
+
+
+
+
+
 
 
 
