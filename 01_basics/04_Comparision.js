c03c7048a38c02c3994c6fdb962a0a6b1bec65a7
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-19 05:13:33 +0000

    comparision operator

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,33 @@
+// COMPARISION OPERATOR
+
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 >= 3);
+// console.log(2 != 3);
+// console.log(2 == 3);
+
+// console.log("2">1);
+// console.log("02">1);        // We do comaprision in same datatypes
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);   
+
+//undefinedd hoo tooo always zero for all comaparision method
+
+// In Js we have treat compariosion operation in two ways like equality & comparison
+
+
+
+console.log(undefined>0);
+console.log(undefined==0);   // null or undefined will be confusion in future use
+console.log(undefined>=0); 
+
+
+
+// === strictly check means it check your datatypes 
+
+console.log("2" === 2);
+
+
