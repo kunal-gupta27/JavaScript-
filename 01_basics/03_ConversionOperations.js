da71547085ec16a700c2893988f71da6a1e43902
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-19 04:26:37 +0000

    conversion of datatype

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+// console.log(typeof score);
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33" => 33(easily convert)
+// "33abs" => NaN
+// when boolean so for true => 1 or false=>0
+
+let isloggedIn = "kunal"  // 0 or 1  0r "" or "kunal"
+
+
+let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(booleanisloggedIn);
+
+// 0 => false;  1=> true;  
+//"" => false; "kunal"=> true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
