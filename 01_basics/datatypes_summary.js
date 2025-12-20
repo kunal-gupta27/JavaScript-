699f600097adc3de413f54ee17f83a6419bdbe33
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-20 04:14:12 +0000

    datatypes Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive datatypes
+
+//7 types => String, Boolean, Number, Null, Undefined, Symbol, BigInt
+
+const score = 33
+const scorevalue = 33.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 88997876786673445556n
+
+//Referencec(Non Primitive)
+
+//Arrays, Objects, Functions    // inka typeof => hamesha object hee aata hain
+
+const heros = ["shaktiman", "nagraj", "doga"]
+
+
+//objects
+let mydetails = {
+    myname : kunal,
+    myage : 20,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction)
+//
