b06a7f047c6001d4ac0e0f6bdc2425ea22b7a8ef
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-23 07:02:43 +0000

    nums & maths

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,49 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(400)  
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) //for creating to decimal 
+
+// const otherNumber = 123.8975
+// console.log(otherNumber.toPrecision(5));   // value will be print from left to right with precision value
+
+// const numbers = 1000000
+// console.log(numbers.toLocaleString());  // by default us based hota hain
+// //for converting to indian number we use this
+// console.log(numbers.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++
+
+// maths library js kai sath by default aati hain
+
+// console.log(Math);
+// console.log(Math.abs(-4));   // change -ve to +ve (work)  or +ve to +ve
+
+// console.log(Math.round(4.7));  // rounding of the number
+
+// console.log(Math.ceil(4.2)) // always choose upper value like 4.2 => 5
+// console.log(Math.floor(4.2)); //always choose lowest value like 4.2 => 4 0r 4.9=>4
+
+// console.log(Math.min(3,4,56,6,2))
+// console.log(Math.max(3,4,56,7,2))
+
+// console.log(Math.random());   // Always come output between 0 to 1
+// console.log(Math.random()*10);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+ min);
+
+
+
+
+
+
+
+
+
