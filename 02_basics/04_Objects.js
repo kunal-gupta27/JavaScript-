6e6a9f92c9ccaffecee0079e593312d0756354d8
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2025-12-28 18:47:06 +0000

    Obj. destructuring & JSON talk

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -6,7 +6,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "sammy"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
 //Object ko hum nested bna sakte hain in js
@@ -21,7 +21,7 @@ const regularUser = {
 }
 
 //Object ko access bhi kr sakte hain
-console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -32,7 +32,7 @@ const obj3 = {5: "a", 6: "b"}
 
 const obj4 = {...obj1, ...obj2, ...obj3}  //latex use 
 
-console.log(obj4);
+// console.log(obj4);
 
 
 //Array kai andar object kitne bhi ho sakte hain
@@ -59,14 +59,50 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser));    //object ko array mai lekr aa jata hahin  or // yai only key print karega
-console.log(Object.values(tinderUser));  // yai key ki value print karega
+// console.log(Object.keys(tinderUser));    //object ko array mai lekr aa jata hahin  or // yai only key print karega
+// console.log(Object.values(tinderUser));  // yai key ki value print karega
 
-console.log(Object.entries(tinderUser));   // entries => array kai andar array banega
+// console.log(Object.entries(tinderUser));   // entries => array kai andar array banega
 
-console.log(tinderUser.hasOwnProperty('isLogged'));  // gives boolean answer
+// console.log(tinderUser.hasOwnProperty('isLogged'));  // gives boolean answer
+
+
+// Object de-Structuring
+
+const course = {
+      coursename: "js in hindi",
+      price: "999",
+      courseInstructor: "tarun"
+
+}
+
+// course.courseInstructor   // isko aese bhi print kra sakta huu
+
+const {courseInstructor: Instructor} = course    //we do here de-structuring in the object property
+
+console.log(Instructor);
+
+// API => application programming interfaces
+// apna kaam dusro sai krvana 
+
+
+//JSON  creation 
+
+// {
+//     "name": "tarun",
+//     "coursename": "js in hindi",
+//     "price": "free"
+
+// }
+
+//JSON jo hain javasript notation hain
+
+
+// Intro about the json using sites =>  best json formatter
+// famous api => Randomuseme
+
 
 
 
