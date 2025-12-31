1fb3b526e04344b43bff01ac4a8ef3fbb5ce924a
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2025-12-31 19:50:08 +0000

    add more function

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -8,6 +8,48 @@ if(true){
     const b = 20;     // if kain andhr vaala block scope hain
     var c = 30;      
 }
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+//Nested Scope
+
+function one(){
+    const username = "tarun"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+     }
+    //  console.log(website);  //it gives you a eror yai jo hain block scope kai andr hain
+
+     two()
+}
+
+one()
+
+if(true){
+    const username="tarun"
+    if(username=="tarun"){
+        const website=" youtube"
+        // console.log(username+website);
+        
+    }
+    // console.log(website);  //yai block scope kai chakkr main error dega
+    
+}
+// console.log(username);  //yai bhi block scope kai chakkr main error dega
+
+//*************************** Interesting ********************************* */
+
+console.log(addone(5))
+function addone(num){
+    return num+1              ///function can be written like this
+}
+// addone(5)
+
+
+console.log(addTwo())
+const addTwo = function(num){   //declaration kai sath variable sai hold karaya hain
+     return num+2       
+}
+// addTwo()     //variable hold kr rakha isliye error dega
