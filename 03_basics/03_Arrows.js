e5bc08a51e105833a523930f7021b7ffa9ebcbb7
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-03 18:37:07 +0000

    No changes

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -1,58 +1,57 @@
-const user = {
-    username: "tarun",
-    price: 999,
+// const user = {
+//     username: "tarun",
+//     price: 999,
 
-    welcomeMessage: function(){
-        console.log(`${this.username} , welcome to website`);   //this keyword cuurent context ko refer krta hain
-        // console.log(this);                       // yai humko name kaisath sath context bhi print karega
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , welcome to website`);   //this keyword cuurent context ko refer krta hain
+//         // console.log(this);                       // yai humko name kaisath sath context bhi print karega
         
         
-    }
-}
+//     }
+// }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
 
-console.log(this);    //global scope main koi bhi context nhi hain
+// console.log(this);    //global scope main koi bhi context nhi hain
 
-function one(){
-    let username = "tarun"
-    console.log(this.username);    //this keyword function main kaam nhi krte only work in object
+// function one(){
+//     let username = "tarun"
+//     console.log(this.username);    //this keyword function main kaam nhi krte only work in object
     
-}
-one()
+// }
+// one()
 
 
-const one = function(){
-    let username = "tarun"
-    console.log(this.username);    // Yaha bhi same print hoga uper vaale function ki tarh
-}
-one()
+// const one = function(){
+//     let username = "tarun"
+//     console.log(this.username);    // Yaha bhi same print hoga uper vaale function ki tarh
+// }
+// one()
 
-// using Arrows function
+// // using Arrows function
 
-const one = () => {     // here we create function using arrow
-    let username="tadung"
-    console.log(this);
+// const one = () => {     // here we create function using arrow
+//     let username="tadung"
+//     console.log(this);
     
-}
-one()
+// }
+// one()
 
-const addTwo = (num1, num2) => {
-    return num1 + num2       //but in eplicit mai return likhna hota hain
-// }   
-console.log(addTwo(3,4))
-//curly braces use kr rhe hain too humko return krna padega
+// const addTwo = (num1, num2) => {
+//     return num1 + num2       //but in eplicit mai return likhna hota hain
+// // }   
+// console.log(addTwo(3,4))
+// //curly braces use kr rhe hain too humko return krna padega
 
-const addTwo = (num1, num2) => num1 + num2     // here we use implicite return  //implicit means=> we don not required to write return
-console.log(addTwo(4,3));
+// const addTwo = (num1, num2) => num1 + num2     // here we use implicite return  //implicit means=> we don not required to write return
+// console.log(addTwo(4,3));
 
 
-// this technique use in React.js
-// const addTwo = (num1, num2) => (num1 + num2)    // isko aese bhi likh sakte hain
+// // this technique use in React.js
+// // const addTwo = (num1, num2) => (num1 + num2)    // isko aese bhi likh sakte hain
 
-const addTwoo = (num1, num2) => ({username:"tarun"})  // object ko aese likha jata hain implicite main
-console.log(addTwoo(3, 4));
-
+// // const addTwoo = (num1, num2) => ({username:"tarun"})  // object ko aese likha jata hain implicite main
+// // console.log(addTwoo(3, 4));
 
