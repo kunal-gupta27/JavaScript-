db2ef127895ff54de0d57a69bf91767d1a10326c
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-05 18:15:33 +0000

    Knowing switches

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+//syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        
+        break;
+    case "feb":
+        console.log("Feb");
+        
+        break;
+    case "March":
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+    case 5:
+        console.log("MAY");
+        
+        break;
+    case 6:
+        console.log("june");
+        
+        break;
+
+    default:
+        console.log("deafult case match");
+        break;
+}
