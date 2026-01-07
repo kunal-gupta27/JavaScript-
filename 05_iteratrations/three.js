6ddf93a77c35f3413c31b92bcd4baeb53df4c003
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-07 18:13:15 +0000

    For of loops

---
+++ 05_iteratrations/three.js
@@ -0,0 +1,21 @@
+// for of loop
+
+//Array apecific loop
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greetings = "Hello World"
+
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+// Maps has unique val
+
+const  map = new Map()
