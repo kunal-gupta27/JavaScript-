d18f7907b5133f58373adf966c2f590e9d9bdcea
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-08 17:42:28 +0000

    basic

---
+++ 05_iteratrations/Six.js
@@ -0,0 +1,7 @@
+const coding=["js","ruby","java","py","cpp"]
+
+const values = coding.forEach((item)=> {
+    console.log(item);
+    
+})
+console.log(values);   //foreach loopvalue ko return nhi krta
