5ab37867a483b88cb8bb2c17ee5d8caa0692ded0
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-08 17:36:26 +0000

    Learning Loops

--- 05_iteratrations/three.js
+++ 05_iteratrations/three.js
@@ -5,17 +5,47 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
     
 }
 
 const greetings = "Hello World"
 
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
     
 }
 
 // Maps has unique val
 
 const  map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);  //maps jo hain unique value kai liye jaane jaate hain
+// means-> koi bhi duplicates value nhi hogi
+//jis order main humne element ko enter kiya vo vaise he raheti hain
+
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);    
+}
+
+// in object 
+
+const myObj = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-', value);    // here myObj variable is not iterable
+// }
+
+
+
+
+
