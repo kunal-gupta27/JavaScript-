0785a993dfffe671f4aae8d6be85bc8484d1bbcd
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-08 17:36:54 +0000

    forof & forin loop

---
+++ 05_iteratrations/four.js
@@ -0,0 +1,31 @@
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+// for (const key in myObj) {
+//     // console.log(myObj[key]);    
+//     console.log(`${key} shortcut for ${myObj[key]}`);
+// }
+
+const programming = ["js","rb","py","java","cpp"]
+
+// for (const key in programming) {
+//    console.log(programming[key]);   
+// }
+
+// const  map = new Map()    // map jo hian iterable nhi hain
+
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(map[key]); // isliye forin loop work nhi karega    
+// }
+
+
+
