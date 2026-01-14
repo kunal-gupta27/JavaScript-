bd2ab5692a6bcb214d70fbf610b45ce5908c12e5
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-01-14 17:34:27 +0000

    no change

--- 05_iteratrations/one.js
+++ 05_iteratrations/one.js
@@ -14,34 +14,34 @@ for (let i = 1; i <= 10; i++) {
     // console.log(`Outer loop value: ${i}`);
     
     for (let j = 1; j <=10; j++) {
-        // console.log(`Inner loop value ${j} and inner loop ${i}`);
-        // console.log(i + '*' + j + ' = ' + i*j);
+        console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + ' = ' + i*j);
         
         
     }
     
 }
 
-// let myArray = ["flash","batman","shaktiman"]
-// console.log(myArray.length);
-// for (let index = 0; index < myArray.length; index++) {
-//     const element = myArray[index];
-//     console.log(element);
-// }
+let myArray = ["flash","batman","shaktiman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
 
 
 // Some Keywords -> break & continue
 
-// for (let index = 1; index <=20; index++) {
-//     if(index==5){
-//         console.log("Detected 5");
-//         break
+for (let index = 1; index <=20; index++) {
+    if(index==5){
+        console.log("Detected 5");
+        break
         
-//     }
-//     console.log(`value of i is ${index}`);
+    }
+    console.log(`value of i is ${index}`);
     
     
-// }
+}
 
 
 for (let index = 1; index <=20; index++) {
